fb8bc33eb29335fdcbb5050ad0305df9ef58ff75
orlando008 <orlando008@gmail.com>
2022-12-14 21:23:28 -0500

    Latest updates

---
+++ Day13/Program.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Data.SqlTypes;
+using System.Security;
+using System.Net.Sockets;
+
+namespace Day13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string List1, string List2)> pairs = new List<(string List1, string List2)>();
+
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string list1 = sr.ReadLine();
+                    string list2 = sr.ReadLine();
+
+                    (string List1, string List2) pair = (list1, list2);
+                    pairs.Add(pair);
+
+                    if (!sr.EndOfStream)
+                    {
+                        sr.ReadLine();
+                    }
+                }
+            }
+
+            ListObj root1 = null;
+            ListObj root2 = null;
+
+            int pairNum = 1;
+
+            int goodPairSum = 0;
+
+            List<ListObj> packets = new List<ListObj>();
+
+            foreach ((string List1, string List2) pair in pairs)
+            {
+                ExtractPairings(ref root1, pair.List1);
+                ExtractPairings(ref root2, pair.List2);
+
+                packets.Add(root1);
+                packets.Add(root2);
+
+                root1 = null;
+                root2 = null;
+            }
+
+            int tot = 0;
+
+        
+
+            for (int i = 0; i < packets.Count; i += 2) if (packets[i].CompareTo(packets[i + 1]) < 0) tot += i / 2 + 1;
+            Console.WriteLine("Good pair sum=" + tot.ToString());
+
+            int ret = 1;
+            List<ListObj> copy = new List<ListObj>(packets);
+
+            List<ListObj> twoList = new List<ListObj>();
+            twoList.Add(new ListObj(null, 2, false));
+            ListObj two = new ListObj(twoList, -1, true);
+
+            List<ListObj> sixList = new List<ListObj>();
+            sixList.Add(new ListObj(null, 6, false));
+            ListObj six = new ListObj(sixList, -1, true);
+
+            copy.Add(two);
+            copy.Add(six);
+            copy.Sort();
+
+            for (int i = 0; i < copy.Count; i++) if (copy[i] == two || copy[i] == six) ret *= i + 1;
+            Console.WriteLine("Decoder Key=" + ret.ToString());
+        }
+
+        private static void ExtractPairings(ref ListObj root, string s)
+        {
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].ToString() == "[")
+                {
+                    if (root == null)
+                    {
+                        root = new ListObj(new List<ListObj>(), -1, isList: true);
+                    }
+                    else
+                    {
+                        ListObj lastUnprocessedList = GetLastUnprocessedListObj(root);
+
+                        if (lastUnprocessedList == null)
+                        {
+                            root.ListInside.Add(new ListObj(new List<ListObj>(), -1, isList: true) { Parent = root });
+                        }
+                        else
+                        {
+                            lastUnprocessedList.ListInside.Add(new ListObj(new List<ListObj>(), -1, isList: true) { Parent = lastUnprocessedList });
+                        }
+                    }
+                }
+                else if (s[i].ToString() == "]")
+                {
+                    ListObj lastUnprocessedList = GetLastUnprocessedListObj(root);
+
+                    if (lastUnprocessedList == null) 
+                    {
+                        Console.WriteLine("TEST");
+                    }
+
+                    //set last list to processed
+                    lastUnprocessedList.IsListClosed = true;
+                }
+                else if (s[i].ToString() == ",")
+                {
+
+                }
+                else
+                {
+                    int number = -1;
+                    string numberString = s[i].ToString();
+
+                    if (int.TryParse(s[i + 1].ToString(),out number))
+                    {
+                        numberString += s[i + 1].ToString();
+                    }
+
+                    ListObj lastUnprocessedList = GetLastUnprocessedListObj(root);
+                    lastUnprocessedList.ListInside.Add(new ListObj(null, Convert.ToInt32(numberString), isList: false) { Parent = lastUnprocessedList });
+
+                    if(numberString.Length == 2)
+                    {
+                        ExtractPairings(ref root, s.Substring(i + 2));
+                        break;
+                    }
+                }
+
+                ExtractPairings(ref root, s.Substring(i + 1));
+                break;
+            }
+
+        }
+
+        private static ListObj GetLastUnprocessedListObj(ListObj root)
+        {
+            if (root.IsList && root.ListInside.Any() && !root.IsListClosed && root.ListInside.Any(b => !b.IsListClosed && b.IsList))
+            {
+                foreach (ListObj list in root.ListInside.Where(b => !b.IsListClosed && b.IsList))
+                {
+                    return GetLastUnprocessedListObj(list);
+                }
+
+                return null;
+            }
+            else if(root.IsList && !root.IsListClosed)
+            {
+                return root;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        class ListObj : IComparable<ListObj>
+        {
+            public List<ListObj> ListInside;
+            public int IntegerNumber;
+            public bool IsList = false;
+            public bool IsListClosed = false;
+            public ListObj Parent;
+            public bool IsInteger
+            {
+                get
+                {
+                    return !IsList;
+                }
+            }
+
+            public ListObj(List<ListObj> listInside, int integerNumber, bool isList)
+            {
+                ListInside = listInside;
+                IntegerNumber = integerNumber;
+                IsList = isList;
+            }
+
+            public override string ToString()
+            {
+                string s = "";
+                if (IsInteger)
+                {
+                    return IntegerNumber.ToString();
+                }
+                else
+                {
+                    s = "[";
+
+                    s += string.Join(",", ListInside);
+
+                    s += "]";
+                }
+
+                return s;
+            }
+
+            public int CompareTo(ListObj? other)
+            {
+                if (ListInside == null && other!.ListInside == null) return IntegerNumber.CompareTo(other.IntegerNumber);
+                if (ListInside != null && other!.ListInside != null)
+                {
+                    int idx = 0;
+                    while (idx < ListInside.Count || idx < other.ListInside.Count)
+                    {
+                        if (idx >= ListInside.Count) return -1;
+                        if (idx >= other.ListInside.Count) return 1;
+                        int ret = ListInside[idx].CompareTo(other.ListInside[idx]);
+                        if (ret != 0) return ret;
+                        idx++;
+                    }
+                    return 0;
+                }
+                if (other!.ListInside != null) return -other.CompareTo(this);
+                if (ListInside != null)
+                {
+                    if (ListInside.Count == 0) return -1;
+                    int ret = ListInside[0].CompareTo(other);
+                    if (ret != 0) return ret;
+                    if (ListInside.Count > 1) return 1;
+                }
+                return 0;
+            }
+        }
+    }
+}
---
+++ Day14/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace Day14
+{
+    internal class Program
+    {
+        const int ROWS = 165;
+        const int COLUMNS = 780;
+
+        static void Main(string[] args)
+        {
+            string[,] cave = new string[ROWS, COLUMNS];
+
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int col = 0; col < COLUMNS; col++)
+                {
+                    cave[row, col] = ".";
+
+                    if (row == ROWS - 1)
+                    {
+                        cave[row, col] = "#";
+                    }
+                }     
+            }
+
+            cave[0, 500] = "+";
+
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    var moves = line.Split("->", StringSplitOptions.RemoveEmptyEntries);
+   
+                    int prevCol = -1;
+                    int prevRow = -1;
+
+                    foreach(string move in moves)
+                    {
+                        int col = Convert.ToInt32(move.Split(",")[0].Trim());
+                        int row = Convert.ToInt32(move.Split(",")[1].Trim());
+                        cave[row, col] = "#";
+
+                        if(prevCol != -1)
+                        {
+                            if(prevCol != col)
+                            {
+                                //do all row values between the columns
+                                for (int i = 1; i < Math.Abs(prevCol - col); i++)
+                                {
+                                    if(prevCol < col)
+                                    {
+                                        cave[row, prevCol + i] = "#";
+                                    }
+                                    else
+                                    {
+                                        cave[row, prevCol - i] = "#";
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                //do all the column values between the rows
+                                for (int i = 1; i < Math.Abs(prevRow - row); i++)
+                                {
+                                    if(prevRow < row)
+                                    {
+                                        cave[prevRow + i, col] = "#";
+                                    }
+                                    else
+                                    {
+                                        cave[prevRow - i, col] = "#";
+                                    }
+                                }
+                            }
+                        }
+
+                        prevCol = col;
+                        prevRow = row;
+                    }
+
+                }
+            }
+
+            PrintCave(cave, 0, ROWS);
+
+            (int col, int row) currentSand = (500, 0);
+
+            int runcount = 0;
+            int totalSandPlace = 0;
+
+            while (true)
+            {
+
+                string blockBelow = cave[currentSand.row + 1, currentSand.col];
+                string blockLeft = cave[currentSand.row + 1, currentSand.col - 1];
+                string blockRight = cave[currentSand.row + 1, currentSand.col + 1];
+
+                if(blockLeft == "O" && blockRight == "O" && blockRight == "O" && currentSand.row == 0)
+                {
+                    PrintCave(cave, 0, ROWS);
+                    break;
+                }
+
+                if (blockBelow == ".")
+                {
+                    currentSand.row += 1;
+                }
+                else if (blockLeft == ".")
+                {
+                    currentSand.col -= 1;
+                    currentSand.row += 1;
+                }
+                else if (blockRight == ".")
+                {
+                    currentSand.col += 1;
+                    currentSand.row += 1;
+                }
+                else
+                {
+                    cave[currentSand.row, currentSand.col] = "O";
+                    currentSand.col = 500;
+                    currentSand.row = 0;
+
+                    totalSandPlace += 1;
+                }
+
+                runcount += 1;
+            }
+
+            Console.WriteLine("Total sand placed: " + totalSandPlace.ToString());
+           
+        }
+
+        static void PrintCave(string[,] cave, int startingColumn, int endingRow)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("   ");
+            for (int i = startingColumn; i < COLUMNS; i++)
+            {
+                sb.Append(i.ToString().PadLeft(3, '0')[0].ToString());
+            }
+
+            sb.AppendLine();
+            sb.Append("   ");
+            for (int i = startingColumn; i < COLUMNS; i++)
+            {
+                sb.Append(i.ToString().PadLeft(3, '0')[1].ToString());
+            }
+
+            sb.AppendLine();
+            sb.Append("   ");
+
+            for (int i = startingColumn; i < COLUMNS; i++)
+            {
+                sb.Append(i.ToString().PadLeft(3, '0')[2].ToString());
+            }
+
+            sb.AppendLine();
+
+            for (int i = 0; i < endingRow; i++)
+            {
+                sb.Append(i.ToString().PadRight(3));
+
+                for (int j = startingColumn; j < COLUMNS; j++)
+                {
+                    sb.Append(cave[i, j]); 
+                }
+
+                sb.AppendLine();
+
+            }
+
+            //Console.WriteLine(sb.ToString());
+            LogToOutput(sb.ToString());
+        }
+
+        static void LogToOutput(string s)
+        {
+            StreamWriter sw = new StreamWriter("output.txt");
+            sw.WriteLine(s);
+            sw.Close();
+        }
+    }
+}
