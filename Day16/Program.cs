8a219e562fe76ca1a4669793a2389efc8148a626
orlando008 <orlando008@gmail.com>
2022-12-18 00:58:26 -0500

    Day 17 couldn't quite get there for part 2.

---
+++ Day16/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.ComponentModel;
+using System.Security;
+
+namespace Day16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string totalString;
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                totalString = sr.ReadToEnd();
+            }
+
+            var strings = totalString.Split("\n");
+
+            Volcano volcano = new Volcano();
+
+            foreach (string s in strings)
+            {
+                var vals = s.Split(";")[0].Replace("Valve ", "").Replace(" has flow rate", "").Split("=");
+                volcano.Valves.Add(new Valve(vals[0], Convert.ToInt32(vals[1])));
+            }
+
+            foreach (string s in strings)
+            {
+                var valveName = s.Split(";")[0].Replace("Valve ", "").Replace(" has flow rate", "").Replace(" ", "").Split("=")[0];
+
+                var vals = s.Split(";")[1].Replace("tunnels", "tunnel").Replace("leads", "lead").Replace("valves", "valve").Replace("tunnel lead to valve ", "").Replace(" ", "").Replace("\r", "").Split(",");
+
+                Valve v = volcano.GetValveWithName(valveName);
+                foreach (string subVal in vals)
+                {
+                    Valve subValve = volcano.GetValveWithName(subVal);
+                    v.AddAdjacent(subValve);
+                }
+            }
+
+
+            Valve currentValve = volcano.Valves[0];
+            Random r = new Random((int)DateTime.Now.Ticks);
+
+            int bestSimulation = 0;
+
+            string simulationString = "";
+
+            List<string> pathsAttempted = new List<string>();
+
+            for (int simulationRound = 0; simulationRound < 500000; simulationRound++)
+            {
+                int totalPressure = 0;
+                currentValve = volcano.Valves[0];
+
+                for (int i = 1; i <= 30; i++)
+                {
+                    //simulationString += "== Minute " + i + " == \n";
+
+                    if (String.IsNullOrEmpty(volcano.GetListOfValvesOpen()))
+                    {
+                        //simulationString += "No valves are open\n";
+                    }
+                    else
+                    {
+                        totalPressure += volcano.GetCurrentPressureOutFlow();
+                        //simulationString += "Valves " + volcano.GetListOfValvesOpen() + " are open, releasing " + volcano.GetCurrentPressureOutFlow().ToString() + " pressure\n";
+                    }
+
+                    if (currentValve.Pressure != 0 && !currentValve.IsOpen)
+                    {
+                        currentValve.IsOpen = true;
+                        //simulationString += currentValve.Name + "\n";
+                    }
+                    else
+                    {
+                        //move
+                        //int valveNum = r.Next(0, currentValve.AdjacentValves.Where(v=>v.Name != simulationString.Substring(simulationString.Count-2)).Count);
+                        //currentValve = currentValve.AdjacentValves.Where(v => v.Name != simulationString.Substring(simulationString.Count - 2))[valveNum];
+                        simulationString += currentValve.Name;
+                       //simulationString += "You move to valve " + currentValve.Name + "\n";
+                    }
+                }
+
+               // simulationString += "Simulation relieved " + totalPressure.ToString() + "\n";
+
+                if (totalPressure > bestSimulation)
+                {
+                    bestSimulation = totalPressure;
+                }
+
+                //using(StreamWriter sw = new StreamWriter("output.txt", append: true))
+                //{
+                //    sw.WriteLine(simulationString);
+                //}
+                pathsAttempted.Add(simulationString);
+                simulationString = "";
+
+
+                foreach (Valve v in volcano.Valves)
+                {
+                    v.IsOpen = false;
+                }
+            }
+
+            Console.WriteLine("Best Simulation relieved " + bestSimulation);
+
+        }
+    }
+
+    class Volcano
+    {
+        public List<Valve> Valves = new List<Valve>();
+
+        public Valve GetValveWithName(string name)
+        {
+            return Valves.FirstOrDefault(v => v.Name == name);
+        }
+
+        public override string ToString()
+        {
+            string s = "----Volcano----\n";
+
+            List<Valve> prevWritten = new List<Valve>();
+            int padCount = 0;
+
+            foreach (Valve v in Valves)
+            {
+                s += v.GetMapString(prevWritten);
+                padCount++;
+            }
+
+            return s;
+        }
+
+        public int GetCurrentPressureOutFlow()
+        {
+            int outflow = 0;
+
+            foreach (Valve v in Valves)
+            {
+                if (v.IsOpen)
+                {
+                    outflow += v.Pressure;
+                }
+            }
+
+            return outflow;
+        }
+
+        public string GetListOfValvesOpen()
+        {
+            return String.Join(",", Valves.Where(v => v.IsOpen).Select(v => v.Name));
+        }
+    }
+
+    class Valve
+    {
+        public string Name;
+        public int Pressure;
+        public List<Valve> AdjacentValves;
+        public bool IsOpen;
+
+        Dictionary<string, int> ValveDistances = new Dictionary<string, int>();
+
+        public Valve(string name, int pressure)
+        {
+            Name = name;
+            Pressure = pressure;
+            AdjacentValves = new List<Valve>();
+        }
+
+        public void AddAdjacent(Valve valve)
+        {
+            AdjacentValves.Add(valve);
+        }
+
+        public string GetMapString(List<Valve> previouslyWrittenValves)
+        {
+            string s = "|" + this.Name + "|\n";
+            s += "|" + this.Pressure.ToString().PadLeft(2, '0') + "|\n";
+
+            foreach (Valve v in AdjacentValves)
+            {
+                s += "|  |";
+            }
+
+            s += "\n";
+
+            foreach (Valve v in AdjacentValves)
+            {
+                s += "|" + v.Name + "|";
+            }
+
+            s += "\n";
+
+            foreach (Valve v in AdjacentValves)
+            {
+                s += "|" + v.Pressure.ToString().PadLeft(2, '0') + "|";
+            }
+
+            s += "\n";
+
+            return s + "\n" + "\n";
+        }
+    }
+}
---
+++ Day17/Program.cs
@@ -0,0 +1,454 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.ComponentModel;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.InteropServices;
+
+namespace Day17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculation must be off slightly, but it's something like this based on the repeating pattern.
+            long answer = (2668 + 2730) + ((long)581395347 * (long)2729) + (long)2290;
+            Console.WriteLine(answer);
+
+            string jetCommands = "";
+
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                jetCommands = sr.ReadToEnd();
+            }
+
+            string originalJetCommands = jetCommands;
+
+            string currentCommand = "new";
+            ChamberOfRocks chamber = new ChamberOfRocks();
+            Rock currentRock = null;
+
+            int rocksThatHaveStopped = 0;
+
+            int iteration = 0;
+
+
+            while (jetCommands != "")
+            {
+                if(currentCommand == "new")
+                {
+                    currentRock = GenerateNewRock(currentRock, chamber);
+                    chamber.Rocks.Add(currentRock);
+                    currentCommand = "gasmove";
+                }
+                else if(currentCommand == "fall")
+                {
+                    bool itFellSuccessfully = PerformFall(currentRock);
+
+                    if(!itFellSuccessfully)
+                    {
+                        currentRock.IsInMotion = false;
+                        currentCommand = "new";
+                        rocksThatHaveStopped += 1;
+
+                        if(iteration > 3 && rocksThatHaveStopped == 1445)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        currentCommand = "gasmove";
+                    }
+                }
+                else
+                {
+                    currentCommand = jetCommands.Substring(0, 1);
+                    PerformGasMove(currentRock, currentCommand);
+
+                    if(jetCommands.Length == 1)
+                    {
+                        jetCommands = originalJetCommands;
+
+                        iteration += 1;
+
+                        Console.WriteLine("Rocks stopped after " + iteration + " commands: " + rocksThatHaveStopped);
+                        Console.WriteLine("Top of tower at " + currentRock.Position.Y);
+                        rocksThatHaveStopped = 0;
+
+                        chamber.PruneChamber();
+
+                        Console.WriteLine("Top of tower now at " + currentRock.Position.Y);
+                    }
+                    else
+                    {
+                        jetCommands = jetCommands.Substring(1);
+                    }
+
+                    currentCommand = "fall";
+                }
+
+            }
+
+           // Console.WriteLine(chamber);
+            Console.WriteLine("Top of tower at " + currentRock.Position.Y);
+        }
+        
+        static Rock GenerateNewRock(Rock previousRock, ChamberOfRocks chamber)
+        {
+            int positionOfFloor = 0;
+
+            if (previousRock != null)
+            {
+                positionOfFloor = previousRock.Position.Y;
+            }
+            
+            if(chamber.Rocks == null)
+            {
+                chamber.Rocks = new List<Rock>();
+            }
+
+            if (previousRock == null || previousRock.GetType() == typeof(FourClumpRock))
+            {
+                return new MinusRock(chamber);
+            }
+            else if (previousRock.GetType() == typeof(MinusRock))
+            {
+                return new PlusRock(chamber);
+            }
+            else if (previousRock.GetType() == typeof(PlusRock))
+            {
+                return new BackwardsLShapeRock(chamber);
+            }
+            else if (previousRock.GetType() == typeof(BackwardsLShapeRock))
+            {
+                return new VertlineRock(chamber);
+            }
+            else
+            {
+                return new FourClumpRock(chamber);
+            }
+        }
+
+        static bool PerformFall(Rock rock)
+        {
+            if (rock.CanMoveInDirection(0, -1))
+            {
+                rock.MoveRockDown();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void PerformGasMove(Rock rock, string gasCommand)
+        {
+            rock.MoveRockBecauseOfGas(gasCommand);
+        }
+        
+        class ChamberOfRocks
+        {
+            public List<Rock> Rocks;
+            public int CHAMBER_WIDTH = 9;
+
+            public int PositionOfFloor
+            {
+                get
+                {
+                    if (Rocks == null || Rocks.Count == 0)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        return Rocks.Max(r => r.Position.Y);
+                    }
+                }
+            }
+
+            public void PruneChamber()
+            {
+                int newFloor = GetNewBaseLineRow();
+
+                Console.WriteLine("New floor = " + newFloor);
+                if(newFloor != 0)
+                {
+                    Rocks.RemoveAll(r => r.Position.Y <= newFloor);
+
+                    foreach (Rock rock in Rocks)
+                    {
+                        rock.Position.Offset(0, -newFloor);
+                    }
+                }
+            }
+
+            public int GetNewBaseLineRow()
+            {
+                bool everyColumnFilled = true;
+
+                for (int row = GetTopOfTower(); row > 0; row--)
+                {
+                    everyColumnFilled = true;
+
+                    for (int column = 1; column < CHAMBER_WIDTH - 1; column++)
+                    {
+                
+                        Rock rockAtPoint = this.IsThereARockAtPoint(new Point(column, row));
+                        if (rockAtPoint == null)
+                        {
+                            everyColumnFilled = false;
+                            break;
+                        }
+                    }
+
+                    if (everyColumnFilled)
+                    {
+                        return row;
+                    }
+                }
+
+                return 0;
+            }
+
+            public Rock IsThereARockAtPoint(Point p)
+            {
+                return Rocks.FirstOrDefault(r => r.GetAllPoints().Any(r1 => r1.X == p.X && r1.Y == p.Y));
+            }
+
+            public int GetTopOfTower()
+            {
+                return Rocks.Max(r => r.Position.Y);
+            }
+
+            public override string ToString()
+            {
+                string s = "";
+
+                for (int row = GetTopOfTower(); row >= 0; row--)
+                {
+                    for (int column = 0; column < CHAMBER_WIDTH; column++)
+                    {
+                        if(column == 0 || column == CHAMBER_WIDTH - 1)
+                        {
+                            s += "|";
+                        }
+                        else if(row == 0)
+                        {
+                            s += "-";
+                        }
+                        else
+                        {
+                            Rock rockAtPoint = this.IsThereARockAtPoint(new Point(column, row));
+                            if(rockAtPoint != null)
+                            {
+                                if (rockAtPoint.IsInMotion)
+                                {
+                                    s += "@";
+                                }
+                                else
+                                {
+                                    s += "#";
+                                }  
+                            }
+                            else
+                            {
+                                s += ".";
+                            }
+                        }
+                    }
+
+                    s += "\n";
+                }
+
+                return s;
+            }
+        }
+
+        class Rock
+        {
+            public enum RockTypeEnum
+            {
+                MINUS = 0,
+                PLUS = 1,
+                BACKWARDSL = 2,
+                VERTLINE = 3,
+                FOURCLUMP = 4
+            }
+
+            public Point Position;
+            public int PositionOfFloor;
+            public bool IsInMotion = true;
+            public ChamberOfRocks ParentChamber;
+
+            public Rock(ChamberOfRocks parentChamber)
+            {
+                this.ParentChamber = parentChamber;
+            }
+
+            public void MoveRockDown()
+            {
+                Position.Offset(0, -1);
+            }
+
+
+            public bool PerformFall()
+            {
+                if(CanMoveInDirection(0, -1))
+                {
+                    this.MoveRockDown();
+                    return true;
+                }
+                else
+                {
+                    this.IsInMotion = false;
+                    return false;
+                }
+            }
+
+            public bool CanMoveInDirection(int dx, int dy)
+            {
+                foreach (Point p in GetAllPoints())
+                {
+                    if (p.X + dx <= 0 || p.X + dx >= ParentChamber.CHAMBER_WIDTH-1)
+                    {
+                        return false;
+                    }
+
+                    if(p.Y + dy <= 0)
+                    {
+                        return false;
+                    }
+
+                    Rock rockAtPoint = ParentChamber.IsThereARockAtPoint(new Point(p.X + dx, p.Y + dy));
+                    if (rockAtPoint != null && rockAtPoint.IsInMotion == false)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            public virtual List<Point> GetAllPoints()
+            {
+                return new List<Point>();
+            }
+
+            public void MoveRockBecauseOfGas(string gasDirection)
+            {
+                if(gasDirection == "<")
+                {
+                    if(CanMoveInDirection(-1, 0))
+                    {
+                        Position.Offset(-1, 0);
+                    }
+                }
+                else
+                {
+                    if(CanMoveInDirection(1, 0))
+                    {
+                        Position.Offset(1, 0);
+                    }
+                }
+            }
+        }
+
+        class MinusRock : Rock
+        {
+            public MinusRock(ChamberOfRocks parentChamber) : base(parentChamber)
+            {
+                this.Position = new Point(3, ParentChamber.PositionOfFloor + 4);
+            }
+
+            public override List<Point> GetAllPoints()
+            {
+                List<Point> points = new List<Point>();
+                points.Add(new Point(this.Position.X, this.Position.Y));
+                points.Add(new Point(this.Position.X+1, this.Position.Y));
+                points.Add(new Point(this.Position.X+2, this.Position.Y));
+                points.Add(new Point(this.Position.X+3, this.Position.Y));
+                return points;
+            }
+        }
+
+        class PlusRock : Rock
+        {
+            public PlusRock(ChamberOfRocks parentChamber) : base(parentChamber)
+            {
+                this.Position = new Point(3, ParentChamber.PositionOfFloor + 6);
+            }
+
+            public override List<Point> GetAllPoints()
+            {
+                List<Point> points = new List<Point>();
+                points.Add(new Point(this.Position.X + 1, this.Position.Y));
+                points.Add(new Point(this.Position.X, this.Position.Y - 1));
+                points.Add(new Point(this.Position.X + 1, this.Position.Y - 1));
+                points.Add(new Point(this.Position.X + 2, this.Position.Y - 1));
+                points.Add(new Point(this.Position.X + 1, this.Position.Y - 2));
+                return points;
+            }
+        }
+
+        class BackwardsLShapeRock : Rock
+        {
+            public BackwardsLShapeRock(ChamberOfRocks parentChamber) : base(parentChamber)
+            {
+                this.Position = new Point(3, ParentChamber.PositionOfFloor + 6);
+            }
+
+            public override List<Point> GetAllPoints()
+            {
+                List<Point> points = new List<Point>();
+                points.Add(new Point(this.Position.X + 2, this.Position.Y));
+                points.Add(new Point(this.Position.X + 2, this.Position.Y - 1));
+                points.Add(new Point(this.Position.X, this.Position.Y - 2));
+                points.Add(new Point(this.Position.X + 1, this.Position.Y - 2));
+                points.Add(new Point(this.Position.X + 2, this.Position.Y - 2));
+                return points;
+            }
+        }
+
+        class VertlineRock : Rock
+        {
+            public VertlineRock(ChamberOfRocks parentChamber) : base(parentChamber)
+            {
+                this.Position = new Point(3, ParentChamber.PositionOfFloor + 7);
+            }
+
+            public override List<Point> GetAllPoints()
+            {
+                List<Point> points = new List<Point>();
+                points.Add(new Point(this.Position.X, this.Position.Y));
+                points.Add(new Point(this.Position.X, this.Position.Y - 1));
+                points.Add(new Point(this.Position.X, this.Position.Y - 2));
+                points.Add(new Point(this.Position.X, this.Position.Y - 3));
+                return points;
+            }
+        }
+
+        class FourClumpRock : Rock
+        {
+            public FourClumpRock(ChamberOfRocks parentChamber) : base(parentChamber)
+            {
+                this.Position = new Point(3, ParentChamber.PositionOfFloor + 5);
+            }
+
+            public override List<Point> GetAllPoints()
+            {
+                List<Point> points = new List<Point>();
+                points.Add(new Point(this.Position.X, this.Position.Y));
+                points.Add(new Point(this.Position.X + 1, this.Position.Y));
+                points.Add(new Point(this.Position.X, this.Position.Y - 1));
+                points.Add(new Point(this.Position.X + 1, this.Position.Y - 1));
+                return points;
+            }
+        }
+    }
+}
